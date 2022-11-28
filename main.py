88e2d802033cc65e15e2cc2b93202f106b0f9f9a
Dawid Płaneta <dawid0planeta@gmail.com>
2022-11-28 18:03:18 +0100

    start

---
+++ main.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+from skimage import data
+from skimage.color import rgb2hsv
+
+rgb_img = data.coffee()
+hsv_img = rgb2hsv(rgb_img)
+hue_img = hsv_img[:, :, 0]
+value_img = hsv_img[:, :, 2]
+
+fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, figsize=(8, 2))
+
+ax0.imshow(rgb_img)
+ax0.set_title("RGB image")
+ax0.axis('off')
+ax1.imshow(hue_img, cmap='hsv')
+ax1.set_title("Hue channel")
+ax1.axis('off')
+ax2.imshow(value_img)
+ax2.set_title("Value channel")
+ax2.axis('off')
+
+fig.tight_layout()
+input()
